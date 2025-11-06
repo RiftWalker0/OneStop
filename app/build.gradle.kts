9e23517423fa08b034d9f7ae7715f399899788d2
Insomniac05 <insomniac22@outlook.in>
2025-11-06 16:49:08 +0530

    bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.onestop"
         minSdk = 26
         targetSdk = 35
-        versionCode = 151
-        versionName = "1.5.1"
+        versionCode = 152
+        versionName = "1.5.2"
         vectorDrawables.useSupportLibrary = true
     }
 
