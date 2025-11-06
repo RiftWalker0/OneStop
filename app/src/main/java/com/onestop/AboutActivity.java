d721551787af6f8e207bab97dd1c762d66090400
Insomniac05 <insomniac22@outlook.in>
2025-11-06 11:30:41 +0530

    Update AboutActivity.java

--- app/src/main/java/com/onestop/AboutActivity.java
+++ app/src/main/java/com/onestop/AboutActivity.java
@@ -2,6 +2,8 @@ package com.onestop;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,22 +24,18 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         TextView tvVersion = findViewById(R.id.tv_version);
-        try {
-            String ver = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
-            try {
-    long code;
-    if (android.os.Build.VERSION.SDK_INT >= 28) {
-        code = getPackageManager().getPackageInfo(getPackageName(), 0).getLongVersionCode();
+try {
+    PackageManager pm = getPackageManager();
+    String pkg = getPackageName();
+    android.content.pm.PackageInfo info;
+    if (Build.VERSION.SDK_INT >= 33) {
+        info = pm.getPackageInfo(pkg, PackageManager.PackageInfoFlags.of(0));
     } else {
-        code = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+        info = pm.getPackageInfo(pkg, 0);
     }
+    String ver = info.versionName;
+    long code = (Build.VERSION.SDK_INT >= 28) ? info.getLongVersionCode() : info.versionCode;
     tvVersion.setText(getString(R.string.version_label) + ": " + ver + " (" + code + ")");
-} catch (Exception e2) {
-    tvVersion.setText(getString(R.string.version_label) + ": " + ver);
-}
-
-        } catch (Exception e) {
-            tvVersion.setText(getString(R.string.version_label) + ": ?");
-        }
-    }
+} catch (Exception e) {
+    tvVersion.setText(getString(R.string.version_label) + ": ?");
 }
